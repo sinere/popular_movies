41ef38941c94a7e01c9798868e4f9c6e97d5d796
sinere <yeefeiooi@gmail.com>
2020-02-07 21:53:57 +0800

    7Feb2020

--- app/src/main/java/com/example/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.popularmovies;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,7 +23,7 @@ import java.util.List;
 
 import static com.example.popularmovies.jsonUtils.parseMovieJson;
 
-public class MainActivity extends AppCompatActivity implements MainActivity.MoviesAdapter.movieAdapterOnClickHandler {
+public class MainActivity extends AppCompatActivity implements MoviesAdapter.movieAdapterOnClickHandler {
 
     private RecyclerView mRecyclerView;
     private MoviesAdapter mAdapter;
@@ -41,19 +40,14 @@ public class MainActivity extends AppCompatActivity implements MainActivity.Movi
         mAdapter = new MoviesAdapter(this, (MoviesAdapter.movieAdapterOnClickHandler) this);
         mRecyclerView.setAdapter(mAdapter);
         new apiQueryTask().execute();
-
     }
 
     @Override
-    public void onClick() {
-
+    public void onClick(Movie indivMovie) {
+        Intent intent = new Intent(this, detail.class);
+        intent.putExtra("Movie", indivMovie);
+        startActivity(intent);
     }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-
 
     class apiQueryTask extends AsyncTask<String, Void, List<Movie>> {
 
@@ -77,82 +71,6 @@ public class MainActivity extends AppCompatActivity implements MainActivity.Movi
         @Override
         protected void onPostExecute(List<Movie> movies) {
             mAdapter.setMovieList(movies);
-        }
-    }
-
-    public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
-    {
-        public ImageView imageView;
-        public TextView mMovieTextView;
-
-        public MovieViewHolder(View itemView)
-        {
-            super(itemView);
-            imageView = (ImageView) itemView.findViewById(R.id.imageView);
-            itemView.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View v) {
-            mClickHandler.onClick();
-        }
-    }
-
-    public static class MoviesAdapter extends RecyclerView.Adapter<MovieViewHolder> {
-        private movieAdapterOnClickHandler mClickHandler;
-        private List<Movie> mMovieList;
-        private LayoutInflater mInflater;
-        private Context mContext;
-
-        public MoviesAdapter(Context context, movieAdapterOnClickHandler mClickHandler)
-        {
-            this.mContext = context;
-            this.mInflater = LayoutInflater.from(context);
-            this.mMovieList = new ArrayList<>();
-            this.mClickHandler = mClickHandler;
-        }
-
-        public MoviesAdapter(MainActivity mainActivity) {
-
-        }
-
-        @Override
-        public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-        {
-            View view = mInflater.inflate(R.layout.movie_layout, parent, false);
-            MovieViewHolder viewHolder = new MovieViewHolder(view);
-            return viewHolder;
-        }
-
-        @Override
-        public void onBindViewHolder(MovieViewHolder holder, int position)
-        {
-            Movie movie = mMovieList.get(position);
-
-            // This is how we use Picasso to load images from the internet.
-            Picasso.get()
-                    .load(movie.getPosterUrl())
-                    .into(holder.imageView);
-            Log.e("CHECK URL", "image url = " + movie.getPosterUrl() );
-        }
-
-        @Override
-        public int getItemCount()
-        {
-            return (mMovieList == null) ? 0 : mMovieList.size();
-        }
-
-        public interface movieAdapterOnClickHandler {
-            void onClick();
-            //Intent intent = new Intent(MainActivity.this, detail.class);
-
-        }
-
-        public void setMovieList(List<Movie> movieList)
-        {
-            this.mMovieList.clear();
-            this.mMovieList.addAll(movieList);
-            notifyDataSetChanged();
         }
     }
 }
